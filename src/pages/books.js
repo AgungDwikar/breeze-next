c23afca749040aee94792127642512122b4090f0
agungdwikar <agungdwika99@gmail.com>
2022-08-01 16:56:49 +0700

    update

--- src/pages/books.js
+++ src/pages/books.js
@@ -5,13 +5,28 @@ import axios from '@/lib/axios'
 
 const bookPage = () => {
     const [books, setBooks] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, seteEror] = useState(null)
+
     const fetchBooks = async () => {
-        const { data } = await axios.get('http://localhost:8000/api/books')
+        // const { data } = await axios.get('http://localhost:8000/api/books')
 
-        // console.log(data.data)
+        // // console.log(data.data)
 
-        // return data.data
-        setBooks(data.data)
+        // // return data.data
+        // setBooks(data.data)
+
+        //cara efisien
+        try {
+            setLoading(true)
+            const { data } = await axios.get('http://localhost:8000/api/books')
+
+            setBooks(data.data)
+        } catch (error) {
+            seteEror(error.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
     useEffect(() => {
@@ -33,11 +48,13 @@ const bookPage = () => {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            {books.map(book => (
-                                <p key={book.id}>
-                                    {book.id} | {book.name}
-                                </p>
-                            ))}
+                            {loading
+                                ? 'Loading....'
+                                : books.map(book => (
+                                      <p key={book.id}>
+                                          {book.id} | {book.name}
+                                      </p>
+                                  ))}
                         </div>
                     </div>
                 </div>
